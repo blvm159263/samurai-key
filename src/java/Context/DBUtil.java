493238480fd0633ea93365e097ee522cfaddd582
blvm159263 <builevanminh0809@gmail.com>
2022-06-26 22:20:56 +0700

    add connection!

---
+++ src/java/Context/DBUtil.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Admin
+ */
+public class DBUtil {
+
+    public Connection getConnection() {
+        String url = "jdbc:sqlserver://localhost;databaseName=PRJShop;user=sa;password=12345";
+        Connection con = null;
+        try {
+            //Loading a driver
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            //Creating a connection
+            con = DriverManager.getConnection(url);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return con;
+
+    }
+    
+    public static void main(String[] args) {
+        DBUtil db = new DBUtil();
+        Connection con = db.getConnection();
+        System.out.println(con);
+    }
+}
+
+
